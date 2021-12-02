59f2c5cd75a56bb4c7cfbd4885ee03b6a04890c4
PyMaster22 <59183571+PyMaster22@users.noreply.github.com>
2021-12-02 10:50:43 -0800

    Update 2.py

--- 2021/2.py
+++ 2021/2.py
@@ -8,3 +8,14 @@ for i in aocin:
 	if(i.split(" ")[0]=="up"):j[0]-=int(i.split(" ")[1])
 	if(i.split(" ")[0]=="down"):j[0]+=int(i.split(" ")[1])
 print(j[0]*j[1])
+
+# Part 2
+j=[0,0]
+aim=0
+for i in aocin:
+	if(i.split(" ")[0]=="forward"):
+		j[1]+=int(i.split(" ")[1])
+		j[0]+=int(i.split(" ")[1])*aim
+	if(i.split(" ")[0]=="up"):aim-=int(i.split(" ")[1])
+	if(i.split(" ")[0]=="down"):aim+=int(i.split(" ")[1])
+print(j[0]*j[1])
